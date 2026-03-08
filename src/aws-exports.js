0409096c9d3b85c22d85f7acbd8d4bfd6f445023
itzMANAV07 <myself.manav07@gmail.com>
2026-03-09 01:11:18 +0530

    fix cognito client id

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,19 +1,11 @@
-const awsconfig = {
+const awsConfig = {
   Auth: {
     Cognito: {
+      region: "ap-south-1",
       userPoolId: process.env.REACT_APP_USER_POOL_ID,
       userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
-      loginWith: {
-        oauth: {
-          domain: 'ap-south-1fiv5myrf3.auth.ap-south-1.amazoncognito.com',
-          scopes: ['email', 'openid', 'profile'],
-          redirectSignIn: ['https://codesikho-zppi.vercel.app/auth/callback'],
-          redirectSignOut: ['https://codesikho-zppi.vercel.app'],
-          responseType: 'code',
-        },
-      },
-    },
-  },
+    }
+  }
 };
 
-export default awsconfig;
+export default awsConfig;
